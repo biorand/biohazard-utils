39884edd1d7d03a0729ca9c58532502642da1646
Ted John <ted@brambles.org>
2025-07-24 03:08:09 +0100

    Name another opcode for RE 1 SCD

--- src/IntelOrca.Biohazard/Script/Bio1ConstantTable.cs
+++ src/IntelOrca.Biohazard/Script/Bio1ConstantTable.cs
@@ -427,7 +427,7 @@ namespace IntelOrca.Biohazard.Script
             "",
             "",
             "",
-            "",
+            "swap_var",
             "",
             "",
             "",
