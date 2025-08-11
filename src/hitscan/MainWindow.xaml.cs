306a68ef760f7bfb3d139e5aacc504bb6c87348e
Ted John <ted@brambles.org>
2025-08-12 00:12:32 +0100

    Start work on hitscan app

---
+++ src/hitscan/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace IntelOrca.Biohazard.HitScan
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
